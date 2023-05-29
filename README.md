82abbc4c16c3470c928d751aa67a1c9eb5edf866
DongyangLi6816 <97556577+DongyangLi6816@users.noreply.github.com>
2023-05-28 17:46:11 -0700

    finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+1. Automated tests should be fitted within a GitHub action that runs whenever code is pushed because it enables continuous integration, providing immediate feedback on code quality. This ensures consistent and standardized testing across the development team, reducing the risk of human errors and inconsistencies.
+2. no End-to-end tests are not used to check if a specific function is returning the correct output. 
+3. No. Becasue message feature is not an individual componenet. It is more appropriate to use End-to-end tests here.
+4. Yes, this is an individual component of a application.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('phone number', () => {
+    expect(functions.isPhoneNumber('(123)-456-7890')).toBe(true);
+});
+
+test('phone number', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('phone number', () => {
+    expect(functions.isPhoneNumber('1ab-afadfsf')).toBe(false);
+});
+
+test('phone number', () => {
+    expect(functions.isPhoneNumber('12345678901')).toBe(false);
+});
+
+test('email', () => {
+    expect(functions.isEmail('dol008@ucsd.edu')).toBe(true);
+});
+
+test('email', () => {
+    expect(functions.isEmail('dongyangli@gmail.com')).toBe(true);
+});
+
+test('email', () => {
+    expect(functions.isEmail('abcdefg')).toBe(false);
+});
+
+test('email', () => {
+    expect(functions.isEmail('1234567890')).toBe(false);
+});
+
+test('password', () => {
+    expect(functions.isStrongPassword('a12345')).toBe(true);
+});
+
+test('password', () => {
+    expect(functions.isStrongPassword('asdfgh1')).toBe(true);
+});
+
+test('password', () => {
+    expect(functions.isStrongPassword('1234567890')).toBe(false);
+});
+
+test('password', () => {
+    expect(functions.isStrongPassword('a1')).toBe(false);
+});
+
+test('date', () => {
+    expect(functions.isDate('01/01/2001')).toBe(true);
+});
+
+test('date', () => {
+    expect(functions.isDate('1/1/2002')).toBe(true);
+});
+
+test('date', () => {
+    expect(functions.isDate('123/1/2001')).toBe(false);
+});
+
+test('date', () => {
+    expect(functions.isDate('12/123/1111')).toBe(false);
+});
+
+test('hex', () => {
+    expect(functions.isHexColor('#000')).toBe(true);
+});
+
+test('hex', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('hex', () => {
+    expect(functions.isHexColor('#11111')).toBe(false);
+});
+
+test('hex', () => {
+    expect(functions.isHexColor('abcdf')).toBe(false);
+});
